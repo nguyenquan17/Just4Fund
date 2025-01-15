9e7e240fdd4e8f472187fd5330a66f66ddd4c7f3
duynm <duy.nguyenmanh@adamosoft.com>
2025-01-15 16:06:11 +0700

    (feat) add rewards page and reward-detail page.

--- router.js
+++ router.js
@@ -2,6 +2,8 @@ const headerUrl = "./layouts/common/AppHeader.html";
 const footerUrl = "./layouts/common/AppFooter.html";
 const homeUrl = "./pages/home/HomePage.html";
 const campaignUrl = "./pages/campaign/CampaignPage.html";
+const rewardsUrl = "./pages/rewards/RewardsPage.html";
+const rewardsDetailUrl = "./pages/rewards/RewardDetailPage.html";
 
 export const layoutDefault = {
   Header: headerUrl,
@@ -11,8 +13,12 @@ const routes = [
   { path: "/", component: () => Promise.resolve(homeUrl) },
   { path: "/campaign", component: () => Promise.resolve(campaignUrl) },
   {
-    path: "/contact",
-    component: () => Promise.resolve("./pages/contact.html"),
+    path: "/rewards",
+    component: () => Promise.resolve(rewardsUrl),
+  },
+  {
+    path: "/reward-detail",
+    component: () => Promise.resolve(rewardsDetailUrl),
   },
 ];
 
